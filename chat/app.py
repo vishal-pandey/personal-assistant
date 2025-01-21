dcc68986c4bafa15e892d73eae3552a95a6cde0f
vishal-pandey <contact@vishalpandey.co.in>
2025-01-22 03:52:41 +0530

    docker compose package

--- chat/app.py
+++ chat/app.py
@@ -2,35 +2,35 @@ import chainlit as cl
 import requests
 import json
 
-chatUrl = "https://n8n.vishalpandey.co.in/webhook/email-chat"
-
-
-
+chatUrl = "http://n8n:5678/webhook/chat"
 
 @cl.on_chat_start
 async def start():
     image = cl.CustomElement(name="mycomponent1")
 
     # Attach the image to the message
-    await cl.Message(
-        content="",
-        elements=[image],
-    ).send()
+    # await cl.Message(
+    #     content="",
+    #     elements=[image],
+    # ).send()
 
 
 @cl.on_message
 async def main(message: cl.Message):
 
+    loading_message = await cl.Message(content="⏳ Loading, please wait...").send()
+
+    session_id = cl.user_session.get("id")
+
     data = requests.post(chatUrl, data={
         "chatInput": message.content,
-        "sessionId": "12345"
+        "sessionId": session_id
     })
     print(data)
 
     data = data.text
     data = json.loads(data)
 
-    await cl.Message(
-        content=data[0]["output"],
-    ).send()
+    loading_message.content = content=data[0]["output"]
+    await loading_message.send()
 
